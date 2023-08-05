8de6a5e12e5e06af5f718551592553b39eb27348
Augusto Cesar <acs-03@hotmail.com>
2023-08-05 13:07:24 -0300

    jsf005-sessionScoped

---
+++ jsf005-sessionScoped/jsf005-sessionScoped/src/main/java/com/acsousa/beans/PessoaBean.java
@@ -0,0 +1,33 @@
+package com.acsousa.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean(name="pessoaBean")
+public class PessoaBean {
+
+	private String nome;
+	private List<String> listaNomes = new ArrayList<>();
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public List<String> getListaNomes() {
+		return listaNomes;
+	}
+	public void setListaNomes(List<String> listaNomes) {
+		this.listaNomes = listaNomes;
+	}
+	
+	public String addNome() {
+		listaNomes.add(nome);
+		return "";
+	}
+}
